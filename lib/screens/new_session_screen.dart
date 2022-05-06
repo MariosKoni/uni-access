b2ce8891fc9549445cebeb5ae420a8ebee34236c
Marios <fanatika.f1@gmail.com>
2022-05-06 19:56:01 +0300

    Made the NFC checking cleaner, by using a future builder.

--- lib/screens/new_session_screen.dart
+++ lib/screens/new_session_screen.dart
@@ -3,49 +3,33 @@ import 'package:nfc_manager/nfc_manager.dart';
 import 'package:flutter_uni_access/widgets/display_new_session_form.dart';
 import 'package:flutter_uni_access/models/uni_user.dart';
 
-class NewSessionScreen extends StatefulWidget {
+class NewSessionScreen extends StatelessWidget {
   final UniUser user;
 
   NewSessionScreen(this.user);
 
   @override
-  State<NewSessionScreen> createState() => _NewSessionScreenState();
-}
-
-class _NewSessionScreenState extends State<NewSessionScreen> {
-  bool _isAvailable = true;
-
-  void _checkNFC() async {
-    final bool _r = await NfcManager.instance.isAvailable();
-    setState(() {
-      _isAvailable = _r;
-    });
-  }
-
-  @override
-  void initState() {
-    _checkNFC();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (_isAvailable) {
-      return const DisplayNewSessionForm();
-    }
-
-    return Center(
-        child: Card(
-      color: Theme.of(context).colorScheme.secondary,
-      elevation: 5,
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(15.0),
-              bottomRight: Radius.circular(15.0))),
-      child: const Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Text('NFC is not available.'),
-      ),
-    ));
+    return FutureBuilder(
+        future: NfcManager.instance.isAvailable(),
+        builder: ((_, AsyncSnapshot snapshot) {
+          if (snapshot.data == true) {
+            return const DisplayNewSessionForm();
+          } else {
+            return Center(
+                child: Card(
+              color: Theme.of(context).colorScheme.secondary,
+              elevation: 5,
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(15.0),
+                      bottomRight: Radius.circular(15.0))),
+              child: const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text('Enable NFC to proceed.'),
+              ),
+            ));
+          }
+        }));
   }
 }
